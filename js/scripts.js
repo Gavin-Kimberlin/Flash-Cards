dd56fe7a7aab163c1c0f7926f06c4d834b4238bc
Gavin Kimberlin and Josh Mabry <gavin@>
2019-06-04 11:43:10 -0700

    create a website with flash cards that reveal the definition when you click on the vocab.

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+  $(".jS").click(function() {
+    $(".jS").fadeToggle();
+  });
+
+  $(".operators").click(function() {
+    $(".operators").fadeToggle();
+  });
+
+  $(".variables").click(function() {
+    $(".variables").fadeToggle();
+  });
+
+  $(".variableName").click(function() {
+    $(".variableName").fadeToggle();
+  });
+
+  $(".functions").click(function() {
+    $(".functions").fadeToggle();
+  });
+
+  $(".methods").click(function() {
+    $(".methods").fadeToggle();
+  });
+
+  $(".arguments").click(function() {
+    $(".arguments").fadeToggle();
+  });
+
+  $(".parameters").click(function() {
+    $(".parameters").fadeToggle();
+  });
+
+  $(".return").click(function() {
+    $(".return").fadeToggle();
+  });
+
+  $(".chaining").click(function() {
+    $(".chaining").fadeToggle();
+  });
+
+  $(".strings").click(function() {
+    $(".strings").fadeToggle();
+  });
+
+  $(".booleans").click(function() {
+    $(".booleans").fadeToggle();
+  });
+
+  $(".undefined").click(function() {
+    $(".undefined").fadeToggle();
+  });
+
+  $(".nan").click(function() {
+    $(".nan").fadeToggle();
+  });
+
+  $(".escape").click(function() {
+    $(".escape").fadeToggle();
+  });
+
+  $(".alert").click(function() {
+    $(".alert").fadeToggle();
+  });
+
+  $(".comments").click(function() {
+    $(".comments").fadeToggle();
+  });
+
+  $(".jquery").click(function() {
+    $(".jquery").fadeToggle();
+  });
+
+  $(".attributes").click(function() {
+    $(".attributes").fadeToggle();
+  });
+});
